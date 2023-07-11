11d2d379ed3f1578c7e2482f7c656e5c72ab2007
ulises25103 <ulises25103@gmail.com>
2023-07-11 11:35:15 -0300

    second commit

--- main.js
+++ main.js
@@ -3,23 +3,21 @@
 
 let edad = prompt("ingrese su edad");
 
-while(edad >= 12){
-    if(edad <= 16){
-        alert("su categoria es menor")
-        break
-    }else if(edad <= 18){
-        alert("su categoria es junior")
-        break
-    } else if(edad < 23){
-        alert("su categoria es sub23")
-        break
-    }else if(edad >= 23 && edad < 28){
-        alert("su categoria es senior")
-        break
-    } else{
-        alert("su categoria es master")
-        break
-    }
+while(edad <= 12){
+    alert("usted no tiene categoria")
+    edad = prompt("Ingresa de nuevo")
+}
+
+if(edad <= 16){
+    alert("su categoria es menor")
+}else if(edad <= 18){
+    alert("su categoria es junior")
+} else if(edad < 23){
+    alert("su categoria es sub23")
+}else if(edad >= 23 && edad < 28){
+    alert("su categoria es senior")
+} else{
+    alert("su categoria es master")
 }
 
 
